9ceac4360d9cdc1b5119d221b55e0f0637f19bef
Strike Digital <59890307+strike-digital@users.noreply.github.com>
2024-03-27 15:13:36 +0000

    Fixed normal maps in imported models for acg assets

--- asset_bridge/apis/ambient_cg/scripts/acg_sc_setup_asset.py
+++ asset_bridge/apis/ambient_cg/scripts/acg_sc_setup_asset.py
@@ -1,7 +1,8 @@
 import argparse
 import os
-from pathlib import Path
 import sys
+from pathlib import Path
+
 import bpy
 
 asset_utils = Path(__file__).parents[2]
@@ -10,7 +11,7 @@ sys.path.append(str(asset_utils))
 parser = argparse.ArgumentParser()
 parser.add_argument("--name")
 parser.add_argument("--output_file")
-args = sys.argv[sys.argv.index("--") + 1:]
+args = sys.argv[sys.argv.index("--") + 1 :]
 args = parser.parse_args(args)
 
 objs = [obj for obj in bpy.data.objects]
@@ -29,6 +30,22 @@ for file in folder.iterdir():
 for obj in bpy.context.scene.objects:
     obj.select_set(True)
     collection.objects.link(obj)
+
+# for mat in bpy.data.materials:
+#     mat: Material
+#     if mat.use_nodes:
+#         for node in mat.node_tree.nodes:
+#             node: bpy.types.Node
+#             if node.bl_idname == "ShaderNodeTexImage":
+#                 if node.outputs[0] and node.outputs[0].links:
+#                     to_node = node.outputs[0].links[0].to_node
+#                     if to_node.bl_idname == "ShaderNodeNormalMap"
+
+for image in bpy.data.images:
+    name_parts = image.name.split("_")
+    if "Color" not in name_parts[-1]:
+        image: bpy.types.Image
+        image.colorspace_settings.is_data = True
 
 bpy.ops.object.shade_smooth()
 
